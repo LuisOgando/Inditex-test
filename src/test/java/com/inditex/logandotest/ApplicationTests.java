4e5f11f81917b6a661434595bed3abf75e60d693
Luis Ogando <logando1193@gmail.com>
2023-04-22 11:59:25 -0400

    Initial commit

---
+++ src/test/java/com/inditex/logandotest/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.logandotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogandotestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
